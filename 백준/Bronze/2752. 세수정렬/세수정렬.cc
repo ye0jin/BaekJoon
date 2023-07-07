8b39319aaf3d545c4f8ac0f7350c5c2f40b3ef95
ye0jin <98932237+ye0jin@users.noreply.github.com>
2023-07-07 12:17:10 +0900

    [Bronze IV] Title: 세수정렬, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+int main()
+{
+	int a[5] = {};
+	for (int i = 0; i < 3; i++)
+	{
+		cin >> a[i];
+	}
+
+	sort(a, a + 3);
+
+	for (int i = 0; i < 3; i++)
+	{
+		cout << a[i] << " ";
+	}
+}
