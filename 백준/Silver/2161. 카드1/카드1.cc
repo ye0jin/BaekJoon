95e9cc7d1520bfe9d1775797d043ea8f1bfe7573
ye0jin <98932237+ye0jin@users.noreply.github.com>
2023-07-11 13:49:10 +0900

    [Silver V] Title: 카드1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2161. 카드1/카드1.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+
+	queue<int> q;
+	
+	for (int i = 1; i <= n; i++)
+	{
+		q.push(i);
+	}
+
+	while (!q.empty())
+	{
+		cout << q.front() << " ";
+		q.pop();
+
+		if (q.empty()) continue;
+		q.push(q.front());
+		q.pop();
+	}
+
+	return 0;
+}
