ec9ff08721f150cee150668d3f31bbed87feeca2
ye0jin <98932237+ye0jin@users.noreply.github.com>
2023-07-17 11:15:58 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+using namespace  std;
+
+int n, m;
+int visit[11] = {};
+int a[11] = {};
+int b[11] = {};
+void check(int j, int v)
+{
+	if (v == m)
+	{
+		for (int i = 0; i < m; i++)
+		{
+			cout << a[i] << " ";
+		}
+		cout << '\n';
+		return;
+	}
+	for (int i = j; i < n; i++)
+	{
+		if (visit[i] == 1) continue;
+		visit[i] = 1;
+		a[v] = b[i];
+		check(i+1, v + 1);
+		visit[i] = 0;
+	}
+}
+int main()
+{
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> b[i];
+	}
+	sort(b, b + n);
+	check(0,0);
+	return 0;
+}
