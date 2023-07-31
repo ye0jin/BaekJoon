980296834bf857d6f5090113a3ffc9d39064b272
ye0jin <98932237+ye0jin@users.noreply.github.com>
2023-07-31 14:23:49 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	string s;
+	s += ".  .   .\n";
+	s += "|  | _ | _. _ ._ _  _\n";
+	s += "|/\\|(/.|(_.(_)[ | )(/.\n";
+
+	cout << s;
+	return 0;
+}
