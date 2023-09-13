b70488a25c1012f1350ade4754a2cdb01d6a9d27
ye0jin <98932237+ye0jin@users.noreply.github.com>
2023-09-13 14:28:45 +0900

    [Silver II] Title: 유기농 배추, Time: 4 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -1,51 +1,39 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
-
-int n, m, k;
-int a[100][100];
-
-void check(int i, int j)
+int t, m, n, k;
+int a[101][101] = {};
+void DFS(int i, int j) 
 {
-	if (i < 0 || j < 0 || i >= n || j >= m || a[i][j] == 0) return;
-
+	if (i < 0 || i >= n || j < 0 || j >= m || a[i][j] == 0) return;
 	a[i][j] = 0;
-	check(i + 1, j);
-	check(i - 1, j);
-	check(i, j + 1);
-	check(i, j - 1);
+	DFS(i-1, j);
+	DFS(i+1, j);
+	DFS(i, j-1);
+	DFS(i, j+1);
 }
 int main()
 {
-	int how;
-	cin >> how;
-	
-	for (int h = 0; h < how; h++)
-	{
+	cin >> t;
+	int x, y;
+	for (int i = 0; i < t; i++) {
 		cin >> m >> n >> k;
-
-		int q = 0;
-		for (int i = 0; i < k; i++)
-		{
-			int b, c;
-			cin >> b >> c;
-			a[c][b] = 1;
+		int cnt = 0;
+		for (int j = 0; j < k; j++) {
+			cin >> x >> y;
+			a[y][x] = 1;
 		}
 
-		for (int i = 0; i < n; i++)
-		{
-			for (int j = 0; j < m; j++)
-			{
-				if (a[i][j] == 1)
-				{
-					q++;
-					check(i, j);
+		for (int j = 0; j < n; j++) {
+			for (int h = 0; h < m; h++) {
+				if (a[j][h] == 1) {
+					cnt++;
+					DFS(j, h);
 				}
 			}
 		}
-		cout << q << '\n';
 
+		cout << cnt << '\n';
 	}
-	
 	return 0;
 }
