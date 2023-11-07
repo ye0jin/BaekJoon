64320a883ef5bee689d2d848a0c3ec15b0047f19
ye0jin <98932237+ye0jin@users.noreply.github.com>
2023-11-07 14:31:31 +0900

    [Silver III] Title: 소수 구하기, Time: 20 ms, Memory: 9844 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -0,0 +1,30 @@
+
+#include <cmath>
+#include<iostream>
+using namespace std;
+
+long long int a[1000001];
+int main(void)
+{
+	long long int n, m;
+	cin >> n >> m;
+	for (long long int i = 2; i <= m; i++)
+		a[i] = i;
+
+	for (long long int i = 2; i <= sqrt(m); i++)
+	{
+		if (a[i] == 0)
+			continue;
+		for (long long int j = i * i; j <= m; j += i)
+			a[j] = 0;
+	}
+
+	for (long long int i = n; i <= m; i++)
+	{
+		if (a[i] == 0)
+			continue;
+		else
+			cout << a[i] << '\n';
+	}
+	return 0;
+}
