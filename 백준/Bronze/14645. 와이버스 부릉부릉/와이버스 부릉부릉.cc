14c19c8c70b12e1e032dcb143da2575e029e5640
ye0jin <98932237+ye0jin@users.noreply.github.com>
2023-12-01 08:36:30 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,k;
+    cin>>n>>k;
+    for(int i=0;i<n;i++)
+    {
+        int a,b;
+        cin>>a>>b;
+    }
+    cout<<"비와이";
+    return 0;
+}
