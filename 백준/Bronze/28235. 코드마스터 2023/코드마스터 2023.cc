7a35f67681cf4a182135f67a137a42382de55cda
ye0jin <98932237+ye0jin@users.noreply.github.com>
2023-12-20 14:42:42 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string a;
+    cin>>a;
+    if(a=="SONGDO") cout<<"HIGHSCHOOL";
+    else if(a=="CODE") cout<<"MASTER";
+    else if(a=="2023") cout<<"0611";
+    else if(a=="ALGORITHM") cout<<"CONTEST";
+    
+    return 0;
+}
