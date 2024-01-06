f6080ce1fe337be6f15093bb591e7772b784be68
ye0jin <98932237+ye0jin@users.noreply.github.com>
2024-01-07 02:04:33 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main() {
+	char a;
+	cin >> a;
+	if (a == 'M')
+	{
+		cout << "MatKor";
+	}
+	else if (a == 'W')
+	{
+		cout << "WiCys";
+	}
+	else if (a == 'C')
+	{
+		cout << "CyKor";
+	}
+	else if (a == 'A')
+	{
+		cout << "AlKor";
+	}
+	else
+	{
+		cout << "$clear";
+	}
+	return 0;
+}
