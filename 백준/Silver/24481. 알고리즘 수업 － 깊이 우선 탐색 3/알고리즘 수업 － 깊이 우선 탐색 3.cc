33c750dca97439b0855d9efa605c68b57ee8b9da
ye0jin <98932237+ye0jin@users.noreply.github.com>
2024-05-16 11:54:14 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 3, Time: 108 ms, Memory: 12880 KB -BaekjoonHub

---
+++ 백준/Silver/24481. 알고리즘 수업 － 깊이 우선 탐색 3/알고리즘 수업 － 깊이 우선 탐색 3.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> a[100001];
+int visit[100001];
+int ans[100001];
+
+void dfs(int x, int depth)
+{
+    ans[x] = depth;
+    visit[x] = 1;
+    for (int i = 0; i < a[x].size(); i++)
+    {
+        if (visit[a[x][i]] == 0)
+        {
+            dfs(a[x][i], depth + 1);
+        }
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n, m, r;
+    cin >> n >> m >> r;
+
+    for (int i = 0; i < m; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+        a[x].push_back(y);
+        a[y].push_back(x);
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        sort(a[i].begin(), a[i].end());
+    }
+
+    dfs(r, 0);
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (visit[i] == 1) cout << ans[i] << '\n';
+        else cout << "-1\n";
+    }
+
+    return 0;
+}
