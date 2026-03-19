616368ffbf416f313ebe86b949acd3c1ebefae34
ye0jin <98932237+ye0jin@users.noreply.github.com>
2026-03-19 11:45:49 +0900

    [Silver V] Title: 중복 빼고 정렬하기, Time: 32 ms, Memory: 2288 KB -BaekjoonHub

---
+++ 백준/Silver/10867. 중복 빼고 정렬하기/중복 빼고 정렬하기.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;++i){
+        cin>>a[i];
+    }
+
+    sort(a,a+n);
+    int prev = 0;
+    
+    for(int i=0;i<n;++i){
+        if(prev == a[i]) continue;    
+        cout<<a[i]<<" ";
+        prev = a[i];
+    }
+    
+    return 0;
+}
