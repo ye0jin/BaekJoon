d829e02f29bddc72e51bfb8e820e7459efe999f2
ye0jin <98932237+ye0jin@users.noreply.github.com>
2026-03-31 16:07:30 +0900

    [Bronze II] Title: 별 찍기 - 21, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10996. 별 찍기 － 21/별 찍기 － 21.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 0;i < n * 2;++i) {
+		for (int j = 0;j < n;++j) {
+			if (i % 2 == j % 2) cout << "*"; 
+			else cout << " ";
+		}
+		cout << endl;
+	}
+}
