f76cf51a9f821710f5533bf0f83862455c28e5bc
ye0jin <98932237+ye0jin@users.noreply.github.com>
2026-04-02 10:20:25 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㄴ, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23803. 골뱅이 찍기 － ㄴ/골뱅이 찍기 － ㄴ.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n;
+	cin >> n;
+	int m = n * 5;
+	for (int i = 0;i < m;++i) {
+		for (int j = 0;j < m;j++) {
+			if (i < m - n)
+			{
+				if (j < n)
+					cout << "@";
+			}
+			else {
+				cout << "@";
+			}
+		}
+		cout << endl;
+	}
+}
